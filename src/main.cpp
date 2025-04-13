f2af4490d3a9f5f31abfc8fbcbce77efe7cae13a
Notchayan <kumawatchayan4@gmail.com>
2025-04-13 21:12:49 +0530

    cleaned repo

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 
 int main() {
-    BufferPoolManager bpm(3);
+    BufferPoolManager bpm(10000);
     std::string table_name = "test_table";
+    std::string table_name2 = "test_table_2";
 
     TableHeap heap(&bpm, table_name);
 
@@ -12,10 +13,21 @@ int main() {
     heap.insertRow("row 3, filling up.");
     heap.insertRow("row 4, maybe new page?");
 
-    
+
     auto all_rows = heap.scanAllRows();
     for (int i = 0; i < all_rows.size(); ++i) {
         std::cout << "Row " << i << ": " << all_rows[i] << "\n";
+    }
+
+    TableHeap heap2(&bpm, table_name2);
+    heap2.insertRow("This is the fucking row 0 of the second table.");
+    heap2.insertRow("This is the fucking row 1 of the second table.");
+    heap2.insertRow("This is the fucking row 2 of the second table.");
+    heap2.insertRow("This is the fucking row 3 of the second table.");
+
+    auto all_rows2 = heap2.scanAllRows();
+    for(int i = 0; i < all_rows2.size(); ++i) {
+        std::cout << "Row " << i << ": " << all_rows2[i] << "\n";
     }
 
     bpm.shutdown();
