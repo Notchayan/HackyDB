ea7caef99c14a2382a18bf747d6e0e10a10df62d
Notchayan <kumawatchayan4@gmail.com>
2025-04-13 22:01:34 +0530

    added B+tree code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,42 @@
 #include "TableHeap/TableHeap.hpp"
 #include "BufferPoolManager/BufferPoolManager.hpp"
 #include <iostream>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> parseRow(const std::string& row) {
+    std::vector<std::string> columns;
+    std::string delimiter = "!@#$";
+    size_t start = 0;
+    size_t end = row.find(delimiter);
+
+    while (end != std::string::npos) {
+        columns.push_back(row.substr(start, end - start));
+        start = end + delimiter.length();
+        end = row.find(delimiter, start);
+    }
+    columns.push_back(row.substr(start));
+    return columns;
+}
 
 int main() {
     BufferPoolManager bpm(10000);
     std::string table_name = "test_table";
-    std::string table_name2 = "test_table_2";
 
     TableHeap heap(&bpm, table_name);
 
-    heap.insertRow("row 2 is also here.");
-    heap.insertRow("row 3, filling up.");
-    heap.insertRow("row 4, maybe new page?");
-
+    heap.insertRow("Col1_data1!@#$Col2_data1!@#$Col3_data1");
+    heap.insertRow("Col1_data2!@#$Col2_data2!@#$Col3_data2");
+    heap.insertRow("Col1_data3!@#$Col2_data3!@#$Col3_data3");
+    heap.insertRow("Col1_data4!@#$Col2_data4!@#$Col3_data4");
 
     auto all_rows = heap.scanAllRows();
-    for (int i = 0; i < all_rows.size(); ++i) {
-        std::cout << "Row " << i << ": " << all_rows[i] << "\n";
-    }
-
-    TableHeap heap2(&bpm, table_name2);
-    heap2.insertRow("This is the fucking row 0 of the second table.");
-    heap2.insertRow("This is the fucking row 1 of the second table.");
-    heap2.insertRow("This is the fucking row 2 of the second table.");
-    heap2.insertRow("This is the fucking row 3 of the second table.");
-
-    auto all_rows2 = heap2.scanAllRows();
-    for(int i = 0; i < all_rows2.size(); ++i) {
-        std::cout << "Row " << i << ": " << all_rows2[i] << "\n";
+    for (const auto& row : all_rows) {
+        auto columns = parseRow(row);
+        for (const auto& col : columns) {
+            std::cout << col << " ";
+        }
+        std::cout << std::endl;
     }
 
     bpm.shutdown();
