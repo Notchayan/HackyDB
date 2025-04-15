a70127b6b138200017bca55cfac8dc9d61375b19
Notchayan <kumawatchayan4@gmail.com>
2025-04-15 17:00:43 +0530

    added headers in RecordManager

--- src/Record_Manager/Record_Manager.hpp
+++ src/Record_Manager/Record_Manager.hpp
@@ -1,4 +1,13 @@
 #ifndef MINIDB_RECORD_MANAGER_H_
 #define MINIDB_RECORD_MANAGER_H_
 
+#include <string>
+#include <vector>
+
+#include "BlockInfo/BlockInfo.hpp"
+#include "BufferManager/BufferManager.hpp"
+#include "CatalogManager/CatalogManager.hpp"
+#include "exceptions.h"
+#include "sql_statement.h"
+
 #endif
