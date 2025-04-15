c628de4a383145154cfb2cdbb58e4a4881909cb6
Notchayan <kumawatchayan4@gmail.com>
2025-04-15 17:02:53 +0530

    defined class structure for RecordManager

--- src/Record_Manager/Record_Manager.hpp
+++ src/Record_Manager/Record_Manager.hpp
@@ -7,7 +7,19 @@
 #include "BlockInfo/BlockInfo.hpp"
 #include "BufferManager/BufferManager.hpp"
 #include "CatalogManager/CatalogManager.hpp"
-#include "exceptions.h"
-#include "sql_statement.h"
+#include "Exceptions/Exceptions.hpp"
+#include "SQLStatement/SQLStatement.hpp"
+
+class  RecordManager {
+    private:
+        BufferManager *hdl_;
+        CatalogManager *cm_;
+        std::string db_name_;
+
+    public:
+
+    RecordManager(CatalogManager *cm, BufferManager *hdl, std::string db) : cm_(cm), hdl_(hdl), db_name_(db) {}
+    ~RecordManager() {}
+};
 
 #endif
